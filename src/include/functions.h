e490f3616311dd7ce6204de0519343bf24e2c3d2
Alex Landherr <alex.landherr@gmail.com>
2023-11-06 18:15:43 +0000

    Cannot solve without a 'Big Integer' library.

--- src/include/functions.h
+++ src/include/functions.h
@@ -1,8 +1,12 @@
-
+#include <string>
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-namespace func {}
+namespace func {
+    std::string add_num_str(std::string str1, std::string str2);
+    std::string multiply_num_str(std::string str1, std::string str2);
+    std::string factorial_str(int n);
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,11 @@ Find the sum of the digits in the number 100!.
 */
 
 int main() {
+    //std::cout << "The sum of the digits in the number 100! is:" << '\n';
+    for (int i = 1; i <= 21; i++) {
+        std::cout << i << "! = " << func::factorial_str(i) << '\n';
+    }
+    //std::cout << func::multiply_num_str("87178291200", "15") << '\n'; //Should be 14! = 14 x 13! = 87 178 291 200.
 
     return 0;
 }
